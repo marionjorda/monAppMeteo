b6a0862a27a82ff87b7ce37c574cb56113bcf4fb
marionjorda <marionjorda@next-u.fr>
2024-03-17 11:56:24 +0100

    météo du jour

---
+++ components/today.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { View, Text, StyleSheet, Image } from "react-native";
+
+
+const Today = (props) => {
+    return (
+        <View style={styles.today}>
+            <Text style={styles.today.title}>Météo du jour :</Text>
+            <Text style={styles.today.text}>{props.data.name} : {props.data.main.temp}°C</Text>
+            <View style={styles.today.container}>
+                <Image style={styles.today.image} source={{uri : `https://openweathermap.org/img/wn/${props.data.weather[0].icon}@2x.png`}}></Image>
+                <Text style={styles.today.description}>{props.data.weather[0].description ? props.data.weather[0].description : 'Aucune description'}</Text>
+            </View>
+        </View>
+    )
+}
+
+
+const styles = StyleSheet.create({
+    today: {
+        backgroundColor: '#B1F1F1',
+        padding: 10,
+        borderRadius: 10,
+        borderColor: '#000000',
+        border: 10,
+        title: {
+          fontSize: 20,
+          fontWeight: 'bold',
+        },
+        text: {
+          fontSize: 15,
+        },
+        description: {
+          fontSize: 13,
+          fontStyle: 'italic',
+          textAlign: 'center',
+        },
+        image: {
+          width: 50,
+          height: 50,
+          backgroundColor: '#199F67',
+          padding: 2,
+          borderRadius: 10,
+          alignSelf: 'center',
+        },
+        container : {
+            margintTop: 10,
+        }
+      },
+})
+
+
+export default Today;
